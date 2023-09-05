4edfb1283ae1fcd1a5aa8771ef52f5690935277c
Aniela Amy <aniela.amy@marklogic.com>
2023-09-05 09:34:19 -0300

    DHFPROD-10161: Entity Validation using global setting.
    (cherry picked from commit 115af8537381d0eec6c95798a228185998d144b7)

--- marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/5/builtins/steps/mapping/validate-entity/shouldValidateEntity.mjs
+++ marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/5/builtins/steps/mapping/validate-entity/shouldValidateEntity.mjs
@@ -7,16 +7,58 @@ function shouldValidate(validateEntityValue) {
   });
 }
 
-[
+function updateGlobalConfigFile(value) {
+  const content = {
+    "entityValidation": value
+  };
+  xdmp.invokeFunction(() => {
+    xdmp.documentInsert("/config/globalSettings.json", content,
+      {
+        permissions: xdmp.defaultPermissions(),
+      })
+  }, { update: "true" });
+}
+
+const assertions = [
+  test.assertFalse(shouldValidate(null), "No values so we should return false"),
+  test.assertFalse(shouldValidate(undefined), "No values so we should return false")
+];
+
+updateGlobalConfigFile("accept");
+
+assertions.concat([
   test.assertTrue(shouldValidate("accept")),
   test.assertTrue(shouldValidate("ACCEPT")),
   test.assertTrue(shouldValidate("reject")),
   test.assertTrue(shouldValidate("REJECT")),
-  test.assertFalse(shouldValidate(null)),
-  test.assertFalse(shouldValidate(undefined)),
+  test.assertTrue(shouldValidate(null), "Null value should return true because global is set to accept"),
+  test.assertTrue(shouldValidate(undefined), "Undefined value should return true because global is set to accept"),
   test.assertFalse(shouldValidate("doNotValidate")),
   test.assertFalse(shouldValidate(false)),
   test.assertFalse(shouldValidate("false")),
   test.assertFalse(shouldValidate(true)),
   test.assertFalse(shouldValidate("true"))
-]
+]);
+
+updateGlobalConfigFile("reject");
+
+assertions.concat([
+  test.assertTrue(shouldValidate("accept")),
+  test.assertTrue(shouldValidate("reject")),
+  test.assertTrue(shouldValidate(null), "Null value should return true because global is set to reject"),
+  test.assertTrue(shouldValidate(undefined), "Undefined value should return true because global is set to reject"),
+  test.assertFalse(shouldValidate("doNotValidate"))
+])
+
+updateGlobalConfigFile("doNotValidate");
+
+assertions.concat([
+  test.assertTrue(shouldValidate("accept")),
+  test.assertTrue(shouldValidate("reject")),
+  test.assertFalse(shouldValidate(null), "Null value should return true because global is set to doNotValidate"),
+  test.assertFalse(shouldValidate(undefined), "Undefined value should return true because global is set to doNotValidate"),
+  test.assertFalse(shouldValidate("doNotValidate"))
+])
+
+
+assertions;
